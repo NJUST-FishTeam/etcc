767bf2c430ed26cfe8a7eaf9b806aace4953b1ea
maemual <maemual@gmail.com>
2015-04-11 17:13:00 +0800

    完善接口

--- api/configure.py
+++ api/configure.py
@@ -4,6 +4,7 @@
 
 import json
 import os
+import shutil
 
 
 import falcon
@@ -13,15 +14,60 @@ from . import config
 
 
 class Collection(object):
-    pass
+
+    def on_get(self, req, resp, service):
+        service_path = os.path.join(config.STORE_PATH, service)
+        if not os.path.exists(service_path):
+            raise falcon.HTTPNotFound
+        files = os.listdir(service_path)
+        configs = [f.split('.')[0] for f in files
+                   if os.path.isfile(os.path.join(service_path, f))]
+
+        data = {}
+        for conf in configs:
+            conf_path = os.path.join(service_path, conf + '.json')
+            data[conf] = json.loads(open(conf_path).read())
+
+        resp.body = json.dumps({
+            'status': 'success',
+            'data': data,
+            'count': len(configs)
+        })
+
+    def on_post(self, req, resp, service):
+        service_path = os.path.join(config.STORE_PATH, service)
+        if not os.path.exists(service_path):
+            raise falcon.HTTPNotFound
+        body = json.loads(req.stream.read())
+        if body['action'] != 'create_configure':
+            resp.status = falcon.HTTP_400
+            resp.body = json.dumps({
+                'status': "failed",
+                'reason': "action is not allowed"
+            })
+            return
+        conf_path = os.path.join(config.STORE_PATH, service, body['configure_name'] + '.json')
+        if os.path.exists(conf_path):
+            resp.status = falcon.HTTP_400
+            resp.body = json.dumps({
+                'status': 'failed',
+                'reason': 'new configure exists'
+            })
+            return
+        conf = open(conf_path, 'w')
+        conf.write(json.dumps(body['configure']))
+        resp.body = json.dumps({
+            'status': "success",
+        })
 
 
 class Item(object):
 
     def on_get(self, req, resp, service, configure):
-        if not os.path.join(config.STORE_PATH, service, configure + ".json"):
+        conf_path = os.path.join(config.STORE_PATH, service, configure + ".json")
+        if not os.path.exists(conf_path):
             raise falcon.HTTPNotFound
-        content = open(os.path.join(config.STORE_PATH, service, configure + ".json"), 'rb').read()
+        content = open(conf_path, 'rb').read()
         resp.body = json.dumps({
             'status': "success",
             'data': json.loads(content),
@@ -33,6 +79,15 @@ class Item(object):
             raise falcon.HTTPNotFound
         conf = open(conf_path, 'w')
         conf.write(req.stream.read())
+        resp.body = json.dumps({
+            'status': "success",
+        })
+
+    def on_delete(self, req, resp, service, configure):
+        conf_path = os.path.join(config.STORE_PATH, service, configure + '.json')
+        if not os.path.exists(conf_path):
+            raise falcon.HTTPNotFound
+        os.remove(conf_path)
         resp.body = json.dumps({
             'status': "success",
         })
