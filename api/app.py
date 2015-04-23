d33db0203062a1e01aca74f28d7ae00a79b29cbc
Comzyh <comzyh@gmail.com>
2015-04-24 03:03:31 +0800

    tiny webserver

--- api/app.py
+++ api/app.py
@@ -7,7 +7,7 @@ import falcon
 
 from api import configure
 from api import service
-
+from api import tinywebserver
 
 api = application = falcon.API()
 
@@ -15,3 +15,9 @@ api.add_route('/services', service.Collection())
 api.add_route('/services/{service}', service.Item())
 api.add_route('/services/{service}/configures', configure.Collection())
 api.add_route('/services/{service}/configures/{configure}', configure.Item())
+
+api.add_route('/',tinywebserver.DefaultPage())
+
+static_file_handler = tinywebserver.StaticFiles()
+api.add_sink(static_file_handler.on_get,'/static/')
+# api.add_route('/static/{filepath}',tinywebserver.StaticFiles())
