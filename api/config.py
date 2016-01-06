d1c6dca89946867a84ca8bd2b0c14db19a98e924
vagrant <comzyh@gmail.com>
2016-01-06 17:07:53 +0000

    Add necessary files

---
+++ api/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+# assume '../config' as configure store
+STORE_PATH = os.path.join(os.getcwd(), 'config')
+
+# you can use absolute path for configure store
+# STORE_PATH = ''
+
+print 'STORE_PATH = ', STORE_PATH
